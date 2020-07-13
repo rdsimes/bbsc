ba41a9f64310a1b9afa9c3e53d97be0d5a572264
Richard Simes <rdsimes@gmail.com>
2020-07-13 17:21:55 +1200

    added the date too

--- bbsc.js
+++ bbsc.js
@@ -2,11 +2,17 @@ fetch('full.json')
 .then(r => r.json())
 .then(data => {
     var table =  $('#Whiskies tbody');
-    data.feed.entry.filter(e => e.gs$cell.col === "1" && e.gs$cell.row != "1")
-    .forEach(e => {
-        console.log(e.content.$t, table[0]);
-        table.append('<tr><td>?</td><td>' +  e.content.$t + '</td></tr>');
+    var names = data.feed.entry.filter(e => e.gs$cell.col === "1" && e.gs$cell.row != "1")
+    .map(e => e.content.$t);
+    var dates = data.feed.entry.filter(e => e.gs$cell.col === "2" && e.gs$cell.row != "1")
+    .map(e => e.content.$t);
+
+    var whiskies = names.map((n, i) => ({name: n, date: dates[i]}));
+
+    whiskies.forEach(w => {
+        table.append('<tr><td>' + w.date + '</td><td>' +  w.name + '</td></tr>')  
     });
+    
     window.data = data;
    
 });
