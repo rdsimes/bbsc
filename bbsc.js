445a1fe557b92b65d46c68ac97e4e95a796a6878
Richard Simes <rdsimes@gmail.com>
2020-07-21 21:18:37 +1200

    installation prompt

--- bbsc.js
+++ bbsc.js
@@ -93,3 +93,41 @@ $('.nav-link').click(l => {
     page = l.target.hash;
     $('#navbarsExampleDefault').removeClass('show');
 });
+
+
+let installPromotion = document.getElementById('install-prompt');
+let buttonInstall = document.getElementById('button-install');
+
+function showInstallPromotion(){
+    installPromotion.classList.toggle('hide');
+}
+
+function hideInstallPromotion(){
+    installPromotion.classList.toggle('hide');
+}
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+buttonInstall.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideInstallPromotion();
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+    });
+  });
