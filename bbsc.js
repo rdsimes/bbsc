865cbfa01126bfec21d83f454daac1504551757f
AzureAD\RichardSimes <richard@gettimely.com>
2022-01-19 21:14:46 +1300

    fixed it

--- bbsc.js
+++ bbsc.js
@@ -1,14 +1,9 @@
-var dataUri = 'https://spreadsheets.google.com/feeds/cells/1IZymUvElVyn4mepHHDWK5VlY_xvC3UVfPOcfb-ohOtE/1/public/full?alt=json'
-//dataUri = 'full.json';
+var dataUri = 'https://whiskydata.azurewebsites.net/api/Data?code=rwScEkC3siQv1ngFevelw0oagPwytaREPOJsZ8Hwd1XHyJIJDcCFUQ=='
+//dataUri = 'functiondata.json';
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js');
   }
 
-function getColumn(data, col)
-{
- return data.feed.entry.filter(e => e.gs$cell.col === col && e.gs$cell.row != "1")
- .map(e => e.content.$t);
-}
 
 function sort(array, field, asc){
     array.sort((a, b) => {
@@ -40,17 +35,15 @@ fetch(dataUri)
     var table =  $('#Whiskies tbody');
     var nextEvent = $('#next-event-data');
     var pastEvents = $('#past-events ul');
-    var names = getColumn(data, "1");
-    var dates = getColumn(data, "2");
     
-    var eventHosts = getColumn(data, "4");
-    var eventDates = getColumn(data, "5");
-    var eventComments = getColumn(data, "6");
+    // var eventHosts = getColumn(data, "4");
+    // var eventDates = getColumn(data, "5");
+    // var eventComments = getColumn(data, "6");
     
 
 
-    var whiskies = names.map((n, i) => ({name: n, date: Date.parse(dates[i])}));
-    var events = eventHosts.map((h, i) => ({host: h, date: Date.parse(eventDates[i]), comment: eventComments[i]}));
+    var whiskies = data.map((n, i) => ({name: n.whisky, date: Date.parse(n.date)}));
+    var events = [] //eventHosts.map((h, i) => ({host: h, date: Date.parse(eventDates[i]), comment: eventComments[i]}));
     var now = new Date();
     var next = events.filter(e => e.date > now)[0];
     var past =  events.filter(e => e.date < now)
